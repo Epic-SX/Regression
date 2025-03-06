72fe1416d6825a4c073212d989b291058c47470c
Epic-SX <blueskyes0501@gmail.com>
2025-03-06 23:17:28 +0800

    fix: modify the vercel.json

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development'
+});
+
+const nextConfig = withPWA({
   env: {
     NEXT_PUBLIC_KOENOTE_API_URL: process.env.NEXT_PUBLIC_KOENOTE_API_URL,
     NEXT_PUBLIC_AUDIO_BUCKET_NAME: process.env.NEXT_PUBLIC_AUDIO_BUCKET_NAME,
-  },
-}
+  }
+});
 
-module.exports = nextConfig 
+module.exports = nextConfig;
